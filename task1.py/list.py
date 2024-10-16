3ab737a951a92fb019dc39ab59b9e9249eedbd0c
Abhinavcv2002 <abhinavcv227@gmail.com>
2024-10-16 09:04:45 +0530

    list

---
+++ task1.py/list.py
@@ -0,0 +1,65 @@
+        # list
+
+ # l=[20,2.35,75]
+ # print(l[1])
+
+        #append list
+
+# l=[20,14.5,51,45]
+# l.append(24)
+# print(l)
+
+        #nexted append
+
+# l=[10,20,30]
+# l.append([15,25])
+# print(l)
+
+        #extend
+
+# l=[10,20,30]
+# l.extend([15,25])
+# print(l)
+
+        #insert
+
+# l=[10,20,30]
+# l.insert(1,123)
+# print(l)
+
+         #pop list
+
+# l=[10,20,30]
+# l.pop(1)
+# print(l) 
+
+        #remove
+
+# l=["apple","orange"]
+# l.remove("apple")
+# print(l)
+
+# l=[12,23,34]
+# l.remove(12)
+# print(l)
+
+       #for using in list
+
+# l=["apple","orange","mango"]
+# for i in l:
+#     print(i)
+
+       #count in list
+
+# l=[10,20,30,40,20,20]
+# print(l.count(20))
+
+# l=[10,20,30,40,20,20]
+# c=l.count(20)
+# print(c)
+
+        #index
+
+# l=[10,20,30]
+# c=l.index(20)
+# print(c)
