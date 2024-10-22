f49ffa8d8346219be1911d0313eab0822d6004f3
Abhinavcv2002 <abhinavcv227@gmail.com>
2024-10-22 09:22:42 +0530

    work

---
+++ task1.py/works/divisible or not.py
@@ -0,0 +1,6 @@
+# to check the last digit of a number divisible by 3 or not
+a=int(input("enter the number"))
+if(a%3==0):
+    print("the number is divisible by 3")
+else:
+    print("the number is not divisible by 3")
