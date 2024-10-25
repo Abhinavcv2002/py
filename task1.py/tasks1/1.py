3a03b9dc049d0c1c16767847a0d9471e5983a816
Abhinavcv2002 <abhinavcv227@gmail.com>
2024-10-25 13:28:23 +0530

    loop

---
+++ task1.py/tasks1/1.py
@@ -0,0 +1,84 @@
+# patterns
+ 
+
+# for i in range(3):
+#     for j in range(3):
+#         print('*',end=" ")
+#     print()
+
+# for i in range(3):
+#     for j in range(1):
+#         print('1',end=" ")
+#         print('2',end=" ")
+#         print('3',end=" ")
+#     print()
+
+# a=int(input("enter the value"))
+# b=int(input("enter the value"))
+# c=int(input("enter the value"))
+# for i in range(3):
+#     for j in range(1):
+#         print(a,b,c,end=" ")
+#     print()
+
+# for i in range(1,4):
+#     for j in range(1,4):
+#         print(j,end=" ")
+#     print()
+
+# for i in range(1,4):
+#     for j in range(1,4):
+#         print(i,end=" ")
+#     print()
+
+# for i in range(1,4):
+#     for j in range(1,11):
+#         print(j,end=" ")
+#     print()
+
+# for i in range(1,11):
+#     for j in range(1,4):
+#         print(i,end="  ")
+#     print()
+
+# n=0
+# for i in range(1,4):
+#     for j in range(1,4):
+#         n=n+1
+#         print(n,end=" ")
+#     print()
+
+# num=1
+# for i in range(3):
+#     for i in range(3):
+#         print(num,end=" ")
+#         num+=1
+#     print()
+
+# for i in range(1,4):
+#     for j in range(1,4):
+#         print(i,end=" ")
+#         i+=1
+#     print()
+
+# for i in range(1,4):
+#     for j in range(i):
+#         print('*',end=" ")
+#     print()
+
+# for i in range(4,1,-1):
+#     for j in range(i):
+#         print('*',end=" ")
+#     print()
+
+# for i in range(1,4):
+#     for j in range(i):
+#         j=j+1
+#         print(j,end=" ")
+#     print()
+
+# for i in range(3,0,-1):
+#     for j in range(i):
+#         j=j+1
+#         print(j,end=" ")
+#     print()
---
+++ task1.py/works/loop work.py
@@ -0,0 +1,19 @@
+    #      * print this type 
+    #    * *
+    #  * * *
+
+for i in range(3,+1):
+    for j in range(i):
+        print('*',end=" ")
+    print()
+
+
+#    *    print this type
+#   * *
+#  * * *
+# * * * *
+
+
+# A B C  print this type
+# D E F
+# G H I
