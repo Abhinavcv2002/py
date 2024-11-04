aa129a40b9d263da7b334a901848b1c01188325e
Abhinavcv2002 <abhinavcv227@gmail.com>
2024-11-04 13:23:38 +0530

    work2

--- task1.py/tasks1/work.py
+++ task1.py/tasks1/work.py
@@ -1,33 +1,102 @@
-while True:
-    print("""
-            1.Add book
-            2.Update book
-            3.Remove book
-            4.View All books
-            5.Search book
-            6.Exit
+# while True:
+#     print("""
+#             1.Add book
+#             2.Update book
+#             3.Remove book
+#             4.View All books
+#             5.Search book
+#             6.Exit
             
-        """)
-    c=int(input("select an option:"))
-    if c==1:
-        l=int(input("how many books :"))
-        book=[]
-        for i in range(1,l+1):
-            print('number of books :',i)
-            snum=input("enter the book number :")
-            sname=input("enter the book name :")
-            book.append({'name':sname,'book number':snum})
-        print(book)
+#         """)
+#     c=int(input("select an option:"))
+#     if c==1:
+#         l=int(input("how many books :"))
+#         book=[]
+#         for i in range(1,l+1):
+#             print('number of books :',i)
+#             snum=input("enter the book number :")
+#             sname=input("enter the book name :")
+#             book.append({'name':sname,'book number':snum})
+#         print(book)
 
-    elif c==2:
-        l=int(input("enter the limit :"))
-        book=[]
-        for i in range(1,l+1):
-            print('number of books :',i)
-            snum=input("enter the book number :")
-            sname=input("enter the book name :")
-            book.append({'name':sname,'book number':snum})
-        print(book)
+#     elif c==2:
+#         l=int(input("enter the limit :"))
+#         book=[]
+#         for i in range(1,l+1):
+#             print('number of books :',i)
+#             snum=input("enter the book number :")
+#             sname=input("enter the book name :")
+#             book.append({'name':sname,'book number':snum})
+#         print(book)
 
-# elif c==3:
-# d= {1: 'abhi', 2: 'vinayak', 3: 'akshay'}
+#------------------------------------------------------------------------------------#
+
+library=[]
+def addbook():
+    book_name=input("enter book name :")
+    writer=input("enter the writername :")
+    year=int(input("enter the year :"))
+    library.append({
+        "name":book_name,
+        "writer":writer,
+        "year":year
+
+    })
+    print("book added successfully")
+
+def updatebook():
+    book_name=input("enter the book name to update")
+    for book in library:
+        if book["name"]==book_name:
+            book["writer"]= input("enter new writer")
+            book["year"]=int(input("enter the year"))
+            print("book updated successfully")
+            return
+        print("book not found")
+
+def removebook():
+    book_name=input("enter the bookname to ramove")
+    for book in library:
+        if book["bookname"]==book_name:
+            library.remove(book)
+            print("book removed successfully")
+            return
+    print("book not found")
+
+def view_book():
+    if library:
+        print("list of all books")
+        for book in library:
+            print(f"name:{book['name']},writer:{book['writer']},year:{book['year']}")
+            print()
+    else:
+        print("no books in library")
+
+
+def main():
+    print("welcome to library")
+    while True:
+        print("""
+                1.add book
+                2.update book
+                3.remove book
+                4.view book
+                5.exit
+              """)
+        choice=input("select any: ")
+        
+        if choice==1:
+            addbook()
+        elif choice==2:
+            updatebook()
+        elif choice==3:
+            removebook()
+        elif choice==4:
+            view_book()
+        elif choice==5:
+            print("exit the library")
+            break
+        else:
+            print("invalid")
+
+main()
---
+++ task1.py/tasks1/work2.py
@@ -0,0 +1,27 @@
+# find the largest and smallest number in the list (using loop method)
+
+# l=[10,1,3,5,8]
+# sma=l[0]
+# for i in l:
+#     if i<sma:
+#         sma=i
+# print("smallest :",sma)
+
+#----------------------------------------#
+# l=[10,1,3,5,8]
+# lar=l[0]
+# for i in l:
+#     if i>lar:
+#         lar=i
+# print("largest  :",lar)
+
+#----------------------------------------#
+
+#find the largest word in the list
+
+# l=["apple","kiwi","orange"]
+# lar=l[0]
+# for i in l:
+#     if len(i)>len(lar):
+#         lar=i
+# print(lar)
