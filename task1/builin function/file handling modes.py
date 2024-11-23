7ea09fba5e277933db57849664e869b42393dd6d
Abhinavcv2002 <abhinavcv227@gmail.com>
2024-11-23 17:55:57 +0530

    file handing

--- task1/builin function/file handling modes.py
+++ task1/builin function/file handling modes.py
@@ -30,3 +30,74 @@
 # x=[10,20]
 # f.write(str(x))
 
+#---------------------------------------------------------------------------------------------------#
+
+# f=open('/home/synnefo/Desktop/py/task1/builin function/demo.txt','r')
+# a=f.read()
+# a=f.read(10)  #  //The 10 c character are print
+# print(a)
+# b=f.read()    #  //the 10 after character are print
+# print(b)
+
+#---------------------------------------------------------------------------------------------------#
+
+# f=open('/home/synnefo/Desktop/py/task1/builin function/demo.txt','r')
+# a=f.readline().strip()
+# print(a)
+# b=f.readline().strip()
+# print(b)
+
+#---------------------------------------------------------------------------------------------------#
+#Lines are print
+
+# f=open('/home/synnefo/Desktop/py/task1/builin function/demo.txt','r')
+# a=f.readlines()
+# print(len(a))
+
+#---------------------------------------------------------------------------------------------------#
+
+# f=open('/home/synnefo/Desktop/py/task1/builin function/demo.txt','r')
+# print(f.tell())
+# f.seek(0)
+# print(f.tell())
+
+#---------------------------------------------------------------------------------------------------#
+#reversed string 
+
+# f=open('/home/synnefo/Desktop/py/task1/builin function/demo.txt','r')
+# a=f.readlines()
+# f.seek(0)
+# for i in range(len(a)):
+#     b=f.readline().strip()
+#     print(b[::-1])
+
+#---------------------------------------------------------------------------------------------------#
+
+# f=open('/home/synnefo/Desktop/py/task1/builin function/demo.txt','r')
+# a=f.readlines()
+# f.seek(0)
+# letter=0
+# for i in range(len(a)):
+#     b=f.readline()
+#     # print(b)
+#     for j in b:
+#         if j!=' ':
+#             letter+=1
+# print('letter',letter)
+
+#---------------------------------------------------------------------------------------------------#
+
+f=open('/home/synnefo/Desktop/py/task1/builin function/demo.txt','r')
+a=f.readlines()
+f.seek(0)
+letter=0
+for i in range(len(a)):
+    b=f.readline()
+    # print(b)
+    for j in b:
+        if j!=' ':
+            letter+=1
+            print('letter',letter)
+
+            
+#---------------------------------------------------------------------------------------------------#
---
+++ task1/builin function/fullmul.py
@@ -0,0 +1,19 @@
+#                                    ///way/// 
+
+# a=int(input("Enter the number :"))
+# f=open("/home/synnefo/Desktop/py/task1/builin function/Mul work\mul1.txt","w")
+# for i in range(1,11):
+#     for j in range(1,a+1):
+#         print("{}*{}*{}".format(i,j,i * j),end="\n")
+#         f.write(str(j)+"*"+str(i)+"="+str(i*j)+"\n")
+
+#--------------------------------------------------------------------------------------------------#
+
+#                                    ///Another way/// 
+
+# f=open('sample.txt','w')
+# no=int(input('enter the number'))
+# for i in range(1,11):
+#     for j in range(1,no+1):
+#         f.write(str(i)+'*'+str(j)+'='+str(i*j)+'\t')
+#     f.write('\n')
